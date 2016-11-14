a1972e2d734b6db2aa9f270b367dd5ec2e0cf6ac
Travis <travis_falls@mac.com>
2016-11-14 16:26:14 -0500

    removed getMapping annotation

--- src/main/java/com/users/controller/IndexController.java
+++ src/main/java/com/users/controller/IndexController.java
@@ -14,7 +14,6 @@ import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.CollectionUtils;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
